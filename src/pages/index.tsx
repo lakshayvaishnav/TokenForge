695d9bd0f0832a3720f70ed7401c277e1c0bcb95
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-08-30 04:25:48 +0530

    styled home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
 
-const index = () => {
-  return <div>index</div>;
+// internal import
+import {
+  HomeView,
+  ToolView,
+  FeatureView,
+  OfferView,
+  FaqView,
+  CreateView,
+  TokenMetadataView,
+  ContactView,
+  AirdropView,
+  DonateView,
+  InputView,
+} from "../views";
+
+const Home: NextPage = (props) => {
+  // state variable
+  const [openCreateModal, setOpenCreateModal] = useState(false);
+  const [openTokenMetaData, setOpenTokenMetaData] = useState(false);
+  const [openContact, setOpenContact] = useState(false);
+  const [opnenAirdrop, setOpenAirdrop] = useState(false);
+  const [openSendTransaction, setOpenSendTransaction] = useState(false);
+
+  return (
+    <>
+      <Head>
+        <title>Solana Token Creator</title>
+        <meta
+          name="solana token creator"
+          content="Build and create solana token"
+        />
+      </Head>
+
+      <HomeView setOpenCreateModal={setOpenCreateModal} />
+
+      {/* <ToolView
+        setOpenAirdrop={setOpenAirdrop}
+        setOpenContact={setOpenContact}
+        setOpenCreateModal={setOpenCreateModal}
+        setOpenSendTransaction={setOpenSendTransaction}
+        setOpenTokenMetaData={setOpenTokenMetaData}
+      />
+
+      <FeatureView
+        setOpenAirdrop={setOpenAirdrop}
+        setOpenContact={setOpenContact}
+        setOpenCreateModal={setOpenCreateModal}
+        setOpenSendTransaction={setOpenSendTransaction}
+        setOpenTokenMetaData={setOpenTokenMetaData}
+      />
+
+      <OfferView />
+
+      <FaqView />
+
+      {openCreateModal && (
+        <div className="new_laoder relative h-full bg-slate-900">
+          <CreateView setOpenCreateModal={setOpenCreateModal} />
+        </div>
+      )}
+
+      {openTokenMetaData && (
+        <div className="new_laoder relative h-full bg-slate-900">
+          <TokenMetadataView setOpenTokenMetaData={setOpenTokenMetaData} />
+        </div>
+      )}
+
+      {openContact && (
+        <div className="new_laoder relative h-full bg-slate-900">
+          <ContactView setOpenContact={setOpenContact} />
+        </div>
+      )}
+
+      {opnenAirdrop && (
+        <div className="new_laoder relative h-full bg-slate-900">
+          <AirdropView setOpenAirdrop={setOpenAirdrop} />
+        </div>
+      )}
+
+      {openSendTransaction && (
+        <div className="new_laoder relative h-full bg-slate-900">
+          <DonateView setOpenSendTransaction={setOpenSendTransaction} />
+        </div>
+      )} */}
+    </>
+  );
 };
 
-export default index;
+export default Home;
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { FC } from "react";
 
-const index = () => {
+import { MdGeneratingTokens } from "react-icons/md";
+
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+import pkg from "../../../package.json";
+
+export const HomeView: FC = ({ setOpenCreateModal }) => {
   return (
-    <div>index</div>
-  )
-}
+    <section id="home" className="relative overflow-hidden  pb-20 pt-[72px]">
+      <div className="px-6 py-4 sm:mt-10">
+        <div className="bg-default-950/40 rounded-2xl">
+          <div className="container">
+            <div className="p-6">
+              <div className="relative grid grid-cols-1 items-center gap-12 lg:grid-cols-2">
+                <div
+                  className="bg-primary/10 -z-1 start-0 absolute top-0 h-14 animate-[spin_10s_linear_infinite] 
+                rounde-2xl rounded-br-none rounded-tl-none"
+                ></div>
+                <div className="bg-primary/20 -z-1 end-0 absolute bottom-0 h-14 w-14 animate-ping rounded-full"></div>
 
-export default index
+                <div>
+                  <span className="text-primary bg-primary/20 rounded-md px-3 py-1 text-sm font-medium uppercase tracking-wider">
+                    CREATE SOLANA TOKEN {pkg.version}
+                  </span>
+
+                  <h1 className="md:text-5xl/tight my-4 max-w-lgf text-4xl font-medium text-white">
+                    Now Create Solana Token To without code
+                  </h1>
+                  <p className="text-default-300 md:text-lg">
+                    Launch your solana token, all in one solana token
+                    developemnt and deployment
+                  </p>
+
+                  <div className="new_add_css">
+                    <a
+                      onClick={() => {
+                        setOpenCreateModal(true);
+                      }}
+                      className="hover:bg-primary-hover mt-6 pe-4 group-first:mt-10 inline-flex 
+                      items-center justify-center gap-2 rounded-full border border-white/10 px-1 pr-3 text-white transition-all duration-500"
+                    >
+                      <span className="bg-primary/20  text-primary me-2 flex h-11 w-11 items-center justify-center rounded-full group-hover:bg-white/10 group-hover:text-white">
+                        <i data-lucide="image">
+                          <MdGeneratingTokens />
+                        </i>
+                      </span>
+                      Create
+                    </a>
+
+                    <a className="mt-8 ">
+                      <WalletMultiButton />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
