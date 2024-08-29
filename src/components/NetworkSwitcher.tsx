2217095f89d19c9170944b900ac7c49a1e0a3c02
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-08-30 00:38:34 +0530

    added notification

--- src/components/NetworkSwitcher.tsx
+++ src/components/NetworkSwitcher.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { FC } from "react";
 
-const NetworkSwitcher = () => {
+import dynamic from "next/dynamic";
+
+// INTERNAL IMPORT
+
+import { useNetworkConfiguration } from "contexts/NetworkConfigurationProvider";
+// import NetworkSwitcher from "./SVG/CreateSVG";
+
+const NetworkSwitcher: FC = () => {
+  const { setNetworkConfiguration, networkConfiguration } =
+    useNetworkConfiguration();
+
   return (
-    <div>NetworkSwitcher</div>
-  )
-}
+    <>
+      <input type="checkbox" id="checkbox" />
+      <label className="switch">
+        <select
+          value={networkConfiguration}
+          onChange={(e) => setNetworkConfiguration(e.target.value || "devnet")}
+          className="select max-w-xs border-none bg-transparent outline-0"
+        >
+          <option value="mainnet-bet" disabled>
+            main
+          </option>
+          <option value="devnet">devnet</option>
+          <option value="testnet" disabled>
+            testnet
+          </option>
+        </select>
+      </label>
+    </>
+  );
+};
 
-export default NetworkSwitcher
+export default dynamic(() => Promise.resolve(NetworkSwitcher), {
+  ssr: false,
+});
